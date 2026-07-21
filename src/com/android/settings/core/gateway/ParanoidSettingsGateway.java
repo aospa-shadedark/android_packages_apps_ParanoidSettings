5355525cb46d6dd336feb0ccb674688d3c20042c
ShadeDark6 <nizamfa6@gmail.com>
2026-07-21 17:25:31 +0000

    Revert "ParanoidSettings: Host Quick Torch settings fragment in PASettingsActivity"
    This reverts commit 27a4c73811a4b936ea8713115fbbf2af0dea248f.

--- src/com/android/settings/core/gateway/ParanoidSettingsGateway.java
+++ src/com/android/settings/core/gateway/ParanoidSettingsGateway.java
@@ -19,7 +19,6 @@ package com.android.settings.core.gateway;
 
 import co.aospa.settings.display.DcDimmingSettings;
 import co.aospa.settings.display.RefreshRateSettings;
-import co.aospa.settings.gestures.PowerButtonTorchSettings;
 
 public class ParanoidSettingsGateway {
 
@@ -29,7 +28,6 @@ public class ParanoidSettingsGateway {
      */
     public static final String[] ENTRY_FRAGMENTS = {
         DcDimmingSettings.class.getName(),
-        RefreshRateSettings.class.getName(),
-        PowerButtonTorchSettings.class.getName(),
+        RefreshRateSettings.class.getName()
     };
 }
